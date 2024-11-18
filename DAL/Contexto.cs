2c203478ffeec41fb47b65a7bb7433f86a0225cd
frailyng <frailindes@gmail.com>
2024-11-18 18:30:16 -0400

    Contexto Modificado con el Model Creating para añadir los articulos

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -11,10 +11,16 @@ namespace FrailynGarcia_AP1_P2.DAL
 
         public DbSet<RegistrosDetalle> RegistrosDetalle { get; set; }
 
+        public DbSet<Articulos> Articulos { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Registros>().HasData(new List<Registros>()
+            modelBuilder.Entity<Articulos>().HasData(new List<Articulos>()
             {
+                new Articulos() {ArticuloId = 1, Descripcion = "Monitor", Costo = 700, Precio = 1400, Existencia = 50},
+                new Articulos() { ArticuloId = 2, Descripcion = "Mouse", Costo = 120, Precio = 500, Existencia = 100},
+                new Articulos() { ArticuloId = 3, Descripcion = "Teclado", Costo = 300, Precio = 1500, Existencia = 70},
+                new Articulos() {ArticuloId = 4, Descripcion = "CPU", Costo = 700, Precio = 1700, Existencia = 50}
 
             });
             base.OnModelCreating(modelBuilder);
