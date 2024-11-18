6194408a7f92d25cb75800c645f999bf4024fbe8
frailyng <frailindes@gmail.com>
2024-11-18 18:21:45 -0400

    Modelo Articulos Creado

---
+++ Models/Articulos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrailynGarcia_AP1_P2.Models;
+
+public class Articulos
+{
+    [Key]
+
+    public int ArticuloId { get; set; }
+
+    [Required(ErrorMessage = "La Descripcion no puede estar en blanco")]
+
+    public string Descripcion { get; set; }
+
+    public double Costo { get; set; }
+
+    public double Precio { get; set; }
+
+    public int Existencia { get; set; }
+}
