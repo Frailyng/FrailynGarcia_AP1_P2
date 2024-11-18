c38a8ddbc816381e40ffa16b2723be7426f9ba07
frailyng <frailindes@gmail.com>
2024-11-18 19:30:35 -0400

    ArticulosService Creado

---
+++ Services/ArticulosService.cs
@@ -0,0 +1,20 @@
+﻿using FrailynGarcia_AP1_P2.DAL;
+using FrailynGarcia_AP1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FrailynGarcia_AP1_P2.Services
+{
+    public class ArticulosService(IDbContextFactory<Contexto> DbFactory)
+    {
+        public async Task<List<ArticulosPC>> Listar(Expression<Func<ArticulosPC, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.ArticulosPC
+                .AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+
+    }
+}
